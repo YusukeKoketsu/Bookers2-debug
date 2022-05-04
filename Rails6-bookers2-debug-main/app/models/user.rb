3b57b1bcde306c77952235ea538e821a9c82566d
YusukeKoketsu <102884584+YusukeKoketsu@users.noreply.github.com>
2022-05-04 12:02:41 +0000

    bookers2-5

--- Rails6-bookers2-debug-main/app/models/user.rb
+++ Rails6-bookers2-debug-main/app/models/user.rb
@@ -10,32 +10,59 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   # フォローをした、されたの関係
+  # 自分がフォローする（与フォロー）側の関係性
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
   # 一覧画面で使う
+   # 与フォロー関係を通じて参照→自分がフォローしている人
   has_many :followings, through: :relationships, source: :followed
+  # 被フォロー関係を通じて参照→自分をフォローしている人
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :introduction, length: { maximum:50 }
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
 
-   # フォローしたときの処理
-def follow(user_id)
-  relationships.create(followed_id: user_id)
-end
-# フォローを外すときの処理
-def unfollow(user_id)
-  relationships.find_by(followed_id: user_id).destroy
-end
-# フォローしているか判定
-def following?(user)
-  followings.include?(user)
-end
-
-
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
+
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
+  # 検索方法分岐
+  def self.looks(search, word)
+    if search == "perfect_match"
+      @user = User.where("name LIKE?", "#{word}")
+
+    elsif search == "partial_match"
+      @user = User.where("name LIKE?","%#{word}%")
+    else
+      @user = User.all
+    end
+  end
+
 end
--- Rails6-bookers2-debug-main/config/routes.rb
+++ Rails6-bookers2-debug-main/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "home/about"=>"homes#about", as: 'about'
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
@@ -16,4 +17,4 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
+  end
